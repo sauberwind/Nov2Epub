7b3cdb5b447f400b4777ca9711a0417cac28e41c
Hiroyuki Goto <sauber@pluto.dti.ne.jp>
2013-05-29 12:21:22 +0900

    StartPoint

--- Nov2Epub/EpubSrcUpdater.cs
+++ Nov2Epub/EpubSrcUpdater.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using System.Xml;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace Nov2Epub
